528e987087ba5a02350beddd35618501416c1ccf
KHa321Z <khammadzaeem@gmail.com>
2025-03-20 20:50:01 +0500

    Actual First Commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const PORT = 5000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Hello From Backend!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Put Shit Here</h1>
+    </>
+  )
+};
+
+export default App;
