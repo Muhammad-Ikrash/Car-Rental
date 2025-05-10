c88b5510ba908ba0d1e78986303a96068cc85b9d
KHa321Z <khammadzaeem@gmail.com>
2025-05-10 22:12:50 +0500

    Eg Check Added

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -12,7 +12,20 @@ router.get("/booking/:bookingID", bookingController.GetBookingDetails);
 router.delete("/booking/:bookingID", bookingController.DeleteSchedule);
 router.post(
     "/booking",
-    [], 
+    [
+        body("clientID").notEmpty().isInt(),
+        body("carID").notEmpty().isInt(),
+        body("driverID").notEmpty().isInt(),
+        body("startDate").notEmpty().isISO8601(),
+        body("endDate").notEmpty().isISO8601(),
+        body("expectedCost").notEmpty().isFloat({ min: 0 }),
+        body("locFrom").notEmpty().isString().isLength({ max: 255 }),
+        body("locTo").notEmpty().isString().isLength({ max: 255 }),
+        body("remarks").notEmpty().isString(),
+        body("promotion").optional({ nullable: true }).isInt(),
+        body("amount").notEmpty().isFloat({ min: 0 }),
+        body("status").notEmpty().isString().isLength({ max: 20 })
+    ], 
     (req, res, next) => {
 
         const errors = validationResult(req);
