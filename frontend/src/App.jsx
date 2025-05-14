68c35e1a3fe59fabc4d1cef95c64e337705f8be9
Coconut-X <aliimranqureshi768@gmail.com>
2025-05-14 15:49:20 +0500

    Added Admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,63 @@
+// import { useState, Suspense, lazy } from 'react';
+// import './App.css';
+// import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+// import LoadingLogo from '../components/LoadingLogo';
+
+// const HomePage = lazy(() => import("../components/HomePage"));
+// const AdminDashboard = lazy(() => import("./components/AdminDashboard"));
+// const BugPage = lazy(() => import("../components/BugPage"));
+// const LoginSignup = lazy(() => import("../components/login"));
+
+// export default function App() {
+//   const [isAuthenticated, setIsAuthenticated] = useState(() => {
+//     const userData = sessionStorage.getItem('user');
+//     return userData ? !!JSON.parse(userData).id : false;
+//   });
+
+//   return (
+//     <div>
+//       <Router>
+//         <Suspense fallback={<LoadingLogo />}>
+//           <Routes>
+//             <Route 
+//               path="/" 
+//               element={isAuthenticated ? <HomePage /> : <Navigate to="/login" replace />} 
+//             />
+//             <Route 
+//               path="/login" 
+//               element={
+//                 isAuthenticated ? (
+//                   <Navigate to="/" replace />
+//                 ) : (
+//                   <LoginSignup onLogin={() => setIsAuthenticated(true)} />
+//                 )
+//               } 
+//             />
+
+            
+          
+
+
+//             <Route path="/bugpage" element={<BugPage />} />
+//           </Routes>
+//         </Suspense>
+//       </Router>
+//     </div>
+//   );
+// }
+
+
+
+
+
 import { useState, Suspense, lazy } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import LoadingLogo from '../components/LoadingLogo';
+import { useEffect } from 'react';
 
 const HomePage = lazy(() => import("../components/HomePage"));
+const Dashboard = lazy(() => import("../components/Dashboard"));
 const AdminDashboard = lazy(() => import("./components/AdminDashboard"));
 const BugPage = lazy(() => import("../components/BugPage"));
 const LoginSignup = lazy(() => import("../components/login"));
@@ -28,6 +82,10 @@ export default function App() {
               element={<AdminDashboard />} 
             />
             <Route 
+              path="/dashboard/:carid" 
+              element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" replace />} 
+            />
+            <Route 
               path="/login" 
               element={
                 isAuthenticated ? (
@@ -37,11 +95,6 @@ export default function App() {
                 )
               } 
             />
-
-            
-          
-
-
             <Route path="/bugpage" element={<BugPage />} />
           </Routes>
         </Suspense>
